ce1204972060d800b1f3de5fb86bcdfbf437a107
charlie <Charliesamuel83@gmail.com>
2025-09-16 14:18:07 +0200

    Add product pricing calculation and user input
    Updated the `Program` class to accept user input for product name, price per unit, and quantity. Implemented total price calculation including VAT and added a new method `CalculateTotalPrice` for this purpose. Display formatted output with product details.

--- Produktpris med metoder/Program.cs
+++ Produktpris med metoder/Program.cs
@@ -5,21 +5,21 @@ namespace Produktpris_med_metoder
     {
         static void Main(string[] args)
         {
-           
+            
             Console.WriteLine("Ange produktnamn");
             string product = Console.ReadLine();
             Console.WriteLine("Ange pris per st");
             double price = GetDouble();
             Console.WriteLine("Ange antal");
             int quantity = GetInt();
+            double total = CalculateTotalPrice(price, quantity);
+            Console.WriteLine($"Produkten {product} med priset {price} kr/st och antal {quantity} st kostar totalt {total} kr inkl. moms");
 
 
-
-
-
-
-
-
+        }
+        private static double CalculateTotalPrice(double price, int quantity, double moms = 0.25)
+        {
+            return price * quantity * (1 + moms);
         }
 
         private static double GetDouble()
