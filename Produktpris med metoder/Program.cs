d17613a8077ee91eaf58f3799f15f965aabfed95
charlie <Charliesamuel83@gmail.com>
2025-09-16 13:52:41 +0200

    Add project files.

---
+++ Produktpris med metoder/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Produktpris_med_metoder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Ange produktnamn");
+            string product = Console.ReadLine();
+            Console.WriteLine("Ange pris per st");
+            double price = GetDouble();
+            Console.WriteLine("Ange antal");
+            int quantity = GetInt();
+
+
+
+
+
+
+
+        }
+
+        private static double GetDouble()
+        {
+            double decimalTal;
+
+            while (double.TryParse(Console.ReadLine(), out decimalTal) == false)
+            {
+                Console.WriteLine("Du har inte angett ett heltal. Försök igen!");
+
+            }
+
+            return decimalTal;
+
+        }
+
+        private static int GetInt()
+        {
+            int helTal;
+
+            while (int.TryParse(Console.ReadLine(), out helTal) == false)
+            {
+                Console.WriteLine("Du har inte angett ett heltal. Försök igen!");
+
+            }
+
+            return helTal;
+
+        }
+
+
+
+
+
+    }
+}
